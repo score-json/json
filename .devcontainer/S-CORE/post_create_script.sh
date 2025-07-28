315cbcff98f2ddd161a53a6f6e500e555d3d07dd
Nils Eberhardt <nils.eberhardt@d-fine.com>
2025-07-28 13:07:07 +0000

    Add cpp test reference for trudag and tests

--- .devcontainer/S-CORE/post_create_script.sh
+++ .devcontainer/S-CORE/post_create_script.sh
@@ -6,3 +6,4 @@ source .venv/bin/activate
 # Install trustable
 pip install --upgrade pip
 pip install trustable --index-url https://gitlab.com/api/v4/projects/66600816/packages/pypi/simple
+pip install pytest
