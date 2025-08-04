e35924a350d3b13376d40702cc898b9314065e50
Nils Eberhardt <nils.eberhardt@d-fine.com>
2025-08-04 13:20:24 +0200

    Restructure doc as code (#22)
    Change the doc-as-code version to 1.0.1:
    
    - The new doc-as-code version adds/renders the CSS correctly
    - The bazel setup is adjusted for the new docs-as-code version 
    - Establish a more explicit separation between nlohmann docs and trustable docs by placing the trustable graph and docs into a new top-level TSF folder (see TSF/README.md`)

--- TSF/scripts/generate_documentation.sh
+++ TSF/scripts/generate_documentation.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-TRUDAG_REPORT_FOLDER="docs/s-core/trustable/generated"
+TRUDAG_REPORT_FOLDER="TSF/docs/generated"
 mkdir -p "$TRUDAG_REPORT_FOLDER"  # -p ensures no error if the folder already exists
 
 trudag publish --validate --output-dir "$TRUDAG_REPORT_FOLDER"
 
 trudag plot -o "$TRUDAG_REPORT_FOLDER/graph.svg"
 
-python3 scripts/clean_trudag_output.py
+python3 TSF/scripts/clean_trudag_output.py
 
-bazel run //docs:incremental
+bazel run //:docs
 
 python3 -m http.server --directory _build
