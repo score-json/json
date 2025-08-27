a39da8e1cf22322fe75063dc527efda71802c55b
Jonas-Kirchhoff <jonas.kirchhoff@d-fine.com>
2025-08-27 11:38:45 +0000

    Logger test 5

--- cmake/ci.cmake
+++ cmake/ci.cmake
@@ -87,7 +87,7 @@ add_custom_target(ci_test_gcc
         -DJSON_BuildTests=ON
         -S${PROJECT_SOURCE_DIR} -B${PROJECT_BINARY_DIR}/build_gcc
     COMMAND ${CMAKE_COMMAND} --build ${PROJECT_BINARY_DIR}/build_gcc
-    COMMAND cd ${PROJECT_BINARY_DIR}/build_gcc && ${CMAKE_CTEST_COMMAND} --parallel ${N} --output-on-failure --output-junit "../my_logs/test.log"
+    COMMAND cd ${PROJECT_BINARY_DIR}/build_gcc && ${CMAKE_CTEST_COMMAND} --parallel ${N} --output-on-failure --output-junit "../my_logs/${GCC_TOOL}_${GCC_CXXFLAGS}.log"
     COMMENT "Compile and test with GCC using maximal warning flags"
 )
 
