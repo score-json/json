ed7c68059ffbc289375b9f7b2ac82236dccf1793
Jonas-Kirchhoff <jonas.kirchhoff@d-fine.com>
2025-08-28 07:21:33 +0000

    test paths

--- TSF/scripts/capture_test_data.py
+++ TSF/scripts/capture_test_data.py
@@ -1,14 +1,17 @@
 import sys
 import sqlite3
+import os.path
 
-connector = sqlite3.connect("TestResultData.db")
-connector.execute("PRAGMA foreign_keys = ON")
-cursor = connector.cursor()
+os.path.isdir("./my_first_artifact")
 
-cursor.execute("CREATE TABLE IF NOT EXISTS workflow_info()")
-cursor.execute("CREATE TABLE IF NOT EXISTS test_results(name TEXT)")
+# connector = sqlite3.connect("TestResultData.db")
+# connector.execute("PRAGMA foreign_keys = ON")
+# cursor = connector.cursor()
+
+# cursor.execute("CREATE TABLE IF NOT EXISTS workflow_info(  status ENUM('successful', 'failed', 'cancelled'))")
+# cursor.execute("CREATE TABLE IF NOT EXISTS test_results(name TEXT)")
 
 
 
-connector.commit()
-connector.close()
+# connector.commit()
+# connector.close()
