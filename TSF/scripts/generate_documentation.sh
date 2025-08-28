b8316b40e0c2f2855134a3a8b792f84e829cf3ff
Mihai Haraga <mihai.haraga@continental-corporation.com>
2025-08-28 11:59:34 +0300

    trustable: Update trudag scripts to work from any folder.

--- TSF/scripts/generate_documentation.sh
+++ TSF/scripts/generate_documentation.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-TRUDAG_REPORT_FOLDER="TSF/docs/generated"
+# global variables
+TRUDAG_SCRIPT_FOLDER=$(dirname "$(realpath $0)")
+TSF_FOLDER="$TRUDAG_SCRIPT_FOLDER/.."
+TRUDAG_REPORT_FOLDER="$TSF_FOLDER/docs/generated"
+
+# generate documentation
+rm -rf "$TRUDAG_REPORT_FOLDER" 
+
 mkdir -p "$TRUDAG_REPORT_FOLDER"  # -p ensures no error if the folder already exists
 
 trudag publish --validate --output-dir "$TRUDAG_REPORT_FOLDER"
 
 trudag plot -o "$TRUDAG_REPORT_FOLDER/graph.svg"
 
-python3 TSF/scripts/clean_trudag_output.py
+python3 "$TRUDAG_SCRIPT_FOLDER/clean_trudag_output.py" "$TRUDAG_REPORT_FOLDER"
 
 bazel run //:docs
 
+# run http server
 python3 -m http.server --directory _build
