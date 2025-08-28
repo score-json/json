c45f8a313f8e471bcaaab0b104687e589c5b05ba
Jonas-Kirchhoff <jonas.kirchhoff@d-fine.com>
2025-08-28 15:45:29 +0000

    Critical test 1

--- TSF/scripts/capture_test_data.py
+++ TSF/scripts/capture_test_data.py
@@ -3,6 +3,7 @@ import sqlite3
 import os
 import xml.etree.ElementTree as ET
 import re
+from datetime import datetime
 
 def setup_environment_variables() -> dict[str, str]:
     """
@@ -68,24 +69,58 @@ try:
 except RuntimeError as e:
     raise RuntimeError("Critical error: Can not uniquely identify environment data! Aborting recording of data.")
 
-for root, dirs, files in os.walk("./thy_artifacts/"):
-    print(root)
-    for dir in dirs: print(dir)
-    for file in files: print(file)
-
 # initiate connection to database
 connector = sqlite3.connect("TestResultData.db")
 connector.execute("PRAGMA foreign_keys = ON")
 cursor = connector.cursor()
 
 # load expected tables
-cursor.execute("CREATE TABLE IF NOT EXISTS workflow_info(repo TEXT, run_id INT, run_attempt INT, status TEXT CHECK(status IN ('successful', 'failed', 'cancelled')) DEFAULT 'failed', PRIMARY KEY (repo, run_id, run_attempt))")
-# cursor.execute("CREATE TABLE IF NOT EXISTS test_results(name TEXT, execution_time REAL, Cpp_standard TEXT, passed_cases INT, failed_cases INT, skipped_cases INT, passed_assertions INT, failed_assertions INT)")
+cursor.execute("CREATE TABLE IF NOT EXISTS workflow_info(repo TEXT, run_id INT, run_attempt INT, status TEXT CHECK(status IN ('successful', 'failed', 'cancelled')) DEFAULT 'failed', PRIMARY KEY(repo, run_id, run_attempt))")
+cursor.execute("CREATE TABLE IF NOT EXISTS test_results(timestamp INT, name TEXT, execution_time REAL, Compiler TEXT, Cpp_standard TEXT, passed_cases INT, failed_cases INT, skipped_cases INT, passed_assertions INT, failed_assertions INT, repo Text, run_id INT, run_attempt INT, PRIMARY KEY(name, timestamp) FOREIGN KEY(repo, run_id, run_attempt) REFERENCES workflow_info)")
 
 # fill in metadata
-command = f"INSERT INTO workflow_info VALUES('{environment.get('GITHUB_REPOSITORY')}', {environment.get('GITHUB_RUN_ID')}, {environment.get('GITHUB_RUN_ID')}, '{sys.argv[1]}')"
+# BEACHTE: This script expects the status of the github workflow as argument
+command = f"INSERT INTO workflow_info VALUES('{environment.get('GITHUB_REPOSITORY')}', {environment.get('GITHUB_RUN_ID')}, {environment.get('GITHUB_RUN_ATTEMPT')}, '{sys.argv[1]}'"
 cursor.execute(command)
+# Don't forget to save!
+connector.commit()
+
+# Load my artifacts
+root, dirs, files = os.walk("./my_artifacts/")
+sqlite_head = "INSERT INTO test_results VALUES"
+# We are only interested in .xml files
+failed_data = []
+for junit_log in (file for file in files if file.endswith('.xml')):
+    tree = ET.parse(junit_log)
+    file_root = tree.getroot()
+    testsuite = next(file_root.iter('testsuite'), None)
+    if testsuite is None:
+        print(f"Error: Could not find testsuite data in {junit_log}.")
+        failed_data.append(junit_log)
+        continue
+    for testcase in (case for case in file_root.iter('testcase') if is_unit_test(case)):
+        metadata = get_metadata(testcase)
+        command = (
+            "INSERT INTO test_results VALUES(",
+             f"{int(datetime.fromisoformat(testsuite.get('timestamp')).timestamp())}, ",
+             f"'{metadata.get('name')}', ",
+             f"{metadata.get('execution time')}, ",
+             f"'{testsuite.get('name')}', ",
+             f"'{metadata.get('standard')}', ",
+             f"{metadata.get('passed test cases')}, ",
+             f"{metadata.get('failed test cases')}, ",
+             f"{metadata.get('skipped test cases')}, ",
+             f"{metadata.get('passed assertions')}, ",
+             f"{metadata.get('failed assertions')}, ",
+             f"'{environment.get('GITHUB_REPOSITORY')}', ",
+             f"{environment.get('GITHUB_RUN_ID')}, ",
+             f"{environment.get('GITHUB_RUN_ATTEMPT')}"
+             ")"
+        )
+        command = "".join(command)
+        cursor.execute(command)
+        connector.commit()
 
 # terminate connection to database
-connector.commit()
+connector.commit() # save, for good measure
 connector.close()
