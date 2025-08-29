56302e89abf315be48900c56e37cbb1d9a3dbaa1
Mihai Haraga <mihai.haraga@continental-corporation.com>
2025-08-29 10:24:39 +0300

    trustable: Review fixes

--- TSF/scripts/generate_documentation.sh
+++ TSF/scripts/generate_documentation.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-# global variables
-TRUDAG_SCRIPT_FOLDER=$(dirname "$(realpath $0)")
-TSF_FOLDER="$TRUDAG_SCRIPT_FOLDER/.."
-TRUDAG_REPORT_FOLDER="$TSF_FOLDER/docs/generated"
+# generate TSF report
+TSF_SCRIPT_FOLDER=$(dirname "$(realpath $0)")
+$TSF_SCRIPT_FOLDER/generate_report.sh
 
-# generate documentation
-rm -rf "$TRUDAG_REPORT_FOLDER" 
-
-mkdir -p "$TRUDAG_REPORT_FOLDER"  # -p ensures no error if the folder already exists
-
-trudag publish --validate --output-dir "$TRUDAG_REPORT_FOLDER"
-
-trudag plot -o "$TRUDAG_REPORT_FOLDER/graph.svg"
-
-python3 "$TRUDAG_SCRIPT_FOLDER/clean_trudag_output.py" "$TRUDAG_REPORT_FOLDER"
-
+# prepare docs
 bazel run //:docs
 
 # run http server
