865410dca69ca4a7ab7f72727341a6278d6d7b16
Jonas-Kirchhoff <jonas.kirchhoff@d-fine.com>
2025-09-05 11:01:11 +0000

    test

--- TSF/scripts/experiment.py
+++ TSF/scripts/experiment.py
@@ -3,7 +3,39 @@ import os
 
 print("Hallo, Welt!")
 
-if os.path.exists("./artifacts"):
-    print("Das war ein voller Erfolg.")
+sha = os.getenv("GITHUB_SHA")
+ubuntu_artifact = f"./artifacts/ubuntu-{str(sha)}"
+if not os.path.exists(ubuntu_artifact):
+    print("Satz mit x.")
 else:
-    print("Das war ein voller Griff ins Klo.")
+    ubuntu_artifact += "/TestResults.db"
+    table = "test_results"
+    connector = sqlite3.connect(ubuntu_artifact)
+    cursor = connector.cursor()
+    # check whether our results can be accessed
+    cursor.execute("SELECT 1 FROM sqlite_master WHERE type='table' AND name=?", (table,))
+    if not cursor.fetchone():
+        # if not, it is not trustable
+        print("Is Scheiße, wa?")
+    else:
+        # our result table can be read
+        tests = ["moin", "test_class_lexer"]
+        score = 0.0
+        expected_tests = len(tests)
+        warnings = []
+        for test in tests:
+            command = f"SELECT COUNT(*) FROM {table} WHERE name = ?"
+            if cursor.execute(command, (test)) is None:
+                warnings.append(Warning(f"Could not find data for test {test}."))
+                continue
+            command = f"""
+                        SELECT
+                            COALESCE(SUM(passed_cases), 0) AS total_passed,
+                            COALESCE(SUM(failed_cases), 0) AS total_failed
+                        FROM {table}
+                        WHERE name = ?
+                    """
+            passed, failed = cursor.execute(command, (test,)).fetchone()
+            score += float(passed)/((float(passed)+float(failed))*expected_tests)
+        print("Toll!")
+        print(score)
