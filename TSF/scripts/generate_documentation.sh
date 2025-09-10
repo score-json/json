8121f806fc9fb02999b92fafa4adc45bbe557c82
Jonas-Kirchhoff <jonas.kirchhoff@d-fine.com>
2025-09-10 07:18:19 +0000

    adapt documentation generator to argument

--- TSF/scripts/generate_documentation.sh
+++ TSF/scripts/generate_documentation.sh
@@ -2,7 +2,7 @@
 
 # generate TSF report
 TSF_SCRIPT_FOLDER=$(dirname "$(realpath $0)")
-$TSF_SCRIPT_FOLDER/generate_report.sh
+$TSF_SCRIPT_FOLDER/generate_report.sh $1
 
 # prepare docs
 bazel run //:docs
