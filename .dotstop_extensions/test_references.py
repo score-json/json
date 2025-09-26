d8fe5eb610cc1a0b9e3a4d28a289662a25617803
Jonas-Kirchhoff <jonas.kirchhoff@d-fine.com>
2025-09-26 13:40:45 +0200

    Review round 2 jk (#93)
    * Add reference to nodes
    
    * add custom reference ItemReference
    * document ItemReference
    
    * add minor tests for ItemReference
    
    * add table for scoring rationale to documentation
    
    * add ItemReferences
    
    For each item referring also to references of other nodes, add
    the corresponding ItemReferences
    
    * change ItemReference
    
    Since each reference's content was calculated, performance was quite bad.
    Therefore, the change of the content is now indirectly detected, only.
    
    * fix orphan-nodes
    
    * PJD-04 already covered by NPFs -> removed PJD-04
    * linked TA-UPDATES and AOU-27
    
    * remove unused code
    
    * re-formulate documentation for clarification
    
    * adapt formulation to be precise
    
    * remove unused pydot
    
    * change import of LocalFileReference to suppress warnings
    
    * add clarification of scope
    
    * improve function-reference
    
    * correct section counting
    
    * fix typo SCORE -> S-CORE

--- .dotstop_extensions/test_references.py
+++ .dotstop_extensions/test_references.py
@@ -2,7 +2,7 @@ import pytest
 import tempfile
 from pathlib import Path
 from unittest.mock import patch
-from references import CPPTestReference, JSONTestsuiteReference, FunctionReference, ListOfTestCases
+from references import CPPTestReference, JSONTestsuiteReference, FunctionReference, ListOfTestCases, ItemReference
 
 
 @pytest.fixture
@@ -563,7 +563,7 @@ def test_check_testsuite_file_is_used_by_cpp_test_missing_file():
         with pytest.raises(ValueError, match="JSON testsuite json_tests/missing.json is not used in the C\\+\\+ test file"):
             JSONTestsuiteReference("test_section", "test.cpp", test_suite_paths, "Test description")
     
-def test_get_function_start():
+def test_get_function_boundaries():
     lines = [
         'template<typename BasicJsonType>\n',
         'class lexer_base\n',
@@ -587,35 +587,9 @@ def test_get_function_start():
         '    }\n',
         '};\n'
     ]
-    assert FunctionReference.get_function_start("foo","lexer::my_function",lines,1) == 16
-
-def test_get_function_end():
-    lines = [
-        'template<typename BasicJsonType>\n',
-        'class lexer_base\n',
-        '{\n',
-        '    // class body\n',
-        '};\n',
-        '\n',
-        'template<typename BasicJsonType, typename InputAdapterType>\n',
-        'class lexer : public lexer_base<BasicJsonType>\n',
-        '{\n',
-        '\n',
-        '  private\n',
-        '    bool dummy_function()\n',
-        '    {\n',
-        '        return my_function();\n',
-        '    }\n',
-        '\n',
-        '    bool my_function()\n',
-        '    {\n',
-        '        // function body \n',
-        '    }\n',
-        '};\n'
-    ]
-    assert FunctionReference.get_function_end("foo","lexer::my_function",16,lines)==19
+    assert FunctionReference.get_function_boundaries("foo","lexer::my_function",lines,1) == [16,19]
     
-def test_get_function_start_with_multiple_overloads():
+def test_get_function_boundaries_with_multiple_overloads():
     lines = [
         'template<typename BasicJsonType>\n',
         'class lexer_base\n',
@@ -649,15 +623,15 @@ def test_get_function_start_with_multiple_overloads():
         '    }\n',
         '};\n'
     ]
-    assert FunctionReference.get_function_start("foo","lexer::my_function",lines,1) == 16
-    assert FunctionReference.get_function_start("foo","lexer::my_function",lines,2) == 21
-    assert FunctionReference.get_function_start("foo","lexer::my_function",lines,3) == 26
+    assert FunctionReference.get_function_boundaries("foo","lexer::my_function",lines,1) == [16,19]
+    assert FunctionReference.get_function_boundaries("foo","lexer::my_function",lines,2) == [21,24]
+    assert FunctionReference.get_function_boundaries("foo","lexer::my_function",lines,3) == [26,29]
     with pytest.raises(ValueError, match="Could not locate 4th implementation of lexer::my_function in file foo."):
-        FunctionReference.get_function_start("foo","lexer::my_function",lines,4)
+        FunctionReference.get_function_boundaries("foo","lexer::my_function",lines,4)
     with pytest.raises(ValueError, match="Could not locate 123rd implementation of lexer::my_function in file foo."):
-        FunctionReference.get_function_start("foo","lexer::my_function",lines,123)
+        FunctionReference.get_function_boundaries("foo","lexer::my_function",lines,123)
     with pytest.raises(ValueError, match="Could not locate 11th implementation of lexer::my_function in file foo."):
-        FunctionReference.get_function_start("foo","lexer::my_function",lines,11)
+        FunctionReference.get_function_boundaries("foo","lexer::my_function",lines,11)
 
 def test_get_function_line_numbers(temp_hpp_file):
     [a,b] = FunctionReference.get_function_line_numbers(str(temp_hpp_file),"lexer::my_function")
@@ -706,3 +680,11 @@ def test_extract_faulty_quotation():
 def test_transform_test_file_to_test_name():
     assert ListOfTestCases.transform_test_file_to_test_name("unit-dummy-test.cpp") == "test-dummy-test"
     assert ListOfTestCases.transform_test_file_to_test_name("unit-dummy_test.cpp") == "test-dummy_test"
+
+def test_faulty_init_ItemReference():
+    with pytest.raises(RuntimeError, match = r"Error: Can't initialise empty ItemReference."):
+        item_reference = ItemReference([])
+
+def test_init_ItemReference():
+    item_reference = ItemReference(["Hallo","Welt"])
+    assert item_reference._items == ["Hallo","Welt"]
--- TSF/trustable/no-json-faults/NJF-12.6.md
+++ TSF/trustable/no-json-faults/NJF-12.6.md
@@ -3,6 +3,9 @@ level: 1.1
 normative: true
 
 references:
+        - type: item
+          items:
+            - JLEX-01
         - type: cpp_test
           name: "Unicode;escaped unicode"
           path: "TSF/tests/unit-strings.cpp"
